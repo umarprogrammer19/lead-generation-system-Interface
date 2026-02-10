cf8d425b4783464b3bf1571857c3678b2c8bcdd6
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-02-10 12:33:14 +0500

    Exception Handling

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+if (!supabaseUrl || !supabaseKey) throw new Error("supabase Keys are not set")
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
